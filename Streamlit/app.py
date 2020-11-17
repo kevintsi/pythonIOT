e2cec29ccdc2e024bb21bdc79f9c69cbb55991f4
Kévin TSI <kevtsiii@gmail.com>
2020-11-17 16:37:11 +0100

    Streamlit update - apres midi

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -1,13 +1,17 @@
+from matplotlib.pyplot import figure
 import streamlit as st
 import numpy as np
 import pandas as pds
 import seaborn as sb
 import matplotlib.pyplot as plt
+import os
+
+data = os.listdir("ML\Data")
 
 
 @st.cache
 def load_data(name):
-    return pds.read_csv("https://raw.githubusercontent.com/kevintsi/pythonIOT/master/ML/Data/"+name+".csv")
+    return pds.read_csv("ML\\Data\\"+name)
 
 
 rad = st.sidebar.radio(
@@ -21,35 +25,42 @@ if rad == "Home":
 if rad == "Affichage des datasets":
     st.title("Affichage des datasets")
     option = st.selectbox("Choisi un dataset",
-                          ("Titanic_Dataset", "weatherAUS"))
-    nb_line = st.text_input("Number of lines : ")
+                          data)
 
-    if nb_line:
-        df = load_data(option)
-        st.subheader("Colonnes : ")
+    df = load_data(option)
+    nb_line = st.number_input(
+        "Nombre de lignes à charger : ", min_value=1, max_value=len(df))
 
-        st.dataframe(df.head(int(nb_line)))
-        df.columns.tolist()
-        st.subheader("Types : ")
+    st.subheader("Affichage des {} premieres lignes : ".format(nb_line))
 
-        st.write(df.dtypes)
+    st.dataframe(df.head(nb_line))
+    st.subheader("Types : ")
 
-        st.subheader("Informations (nombre de lignes et de colonnes) : ")
+    st.write(df.dtypes)
 
-        st.write("Il y a {} lignes et {} colonnes pour la dataset Titanic".format(
-            df.shape[0], df.shape[1]))
+    selected_type = st.selectbox(
+        "Choisi une colonne : ", df.columns)
 
-        st.subheader("Statistiques : ")
+    st.write("Le type de {} est {} ".format(
+        selected_type, df[selected_type].dtype))
 
-        st.write("Les statistiques du dataset Titanic : ")
-        st.write(df.describe())
+    st.subheader("Informations (nombre de lignes et de colonnes) : ")
+
+    st.write("Il y a {} lignes et {} colonnes pour le dataset".format(
+        df.shape[0], df.shape[1]))
+
+    st.subheader("Statistiques : ")
+
+    st.write("Les statistiques du dataset : ")
+    st.write(df.describe())
 
 if rad == "Visualisation graphique":
     st.title("Visualisation graphique")
     option = st.selectbox("Choisi un dataset",
-                          ("Titanic_Dataset", "weatherAUS"))
+                          data)
     df = load_data(option)
-    st.write(sb.heatmap(df.corr(), annot=True))
+    plt.figure(figsize=(15, 10))
+    st.write(sb.heatmap(df.corr(), annot=True, linewidths=.30))
     st.pyplot()
     try:
         st.write(plt.hist(df))
