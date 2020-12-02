515154f14fc0816e33f7b145d2c6a039ed20d85f
Kevin TSI <kevtsiii@gmail.com>
2020-12-02 12:00:39 +0100

    Update app.py, Dockerfile for deployment heroku and update cheat sheet

--- Flask/TP1/app.py
+++ Flask/TP1/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify
 import json
+import os
 
 app = Flask(__name__)
 
@@ -70,4 +71,4 @@ def get_book_by_title(title):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
